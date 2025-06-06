b5cb0fa1f026f9aad9ea515dbc6f1512628ba3a1
Taylor Hill <taylorhill@gmail.com>
2025-06-05 21:15:44 -0400

    no message

--- test_dependencies.py
+++ test_dependencies.py
@@ -101,3 +101,10 @@ def print_dependency_check_results(results):
 
     if (results['huggingface_access'] is False or results['model_access'] is False):
         print("➡️  Run 'huggingface-cli login' in your terminal and ensure you have access to the model.")
+
+
+if __name__ == "__main__":
+    all_passed, results = check_dependencies(model_name=training_config.model_name,
+                                 ignore_cuda=False,
+                                 ignore_huggingface=True)
+    print_dependency_check_results(results)
